3aee1ba89559d2674c0681e5ae5ed1fe22611c14
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-24 15:23:03 -0300

    tail footer component of the application

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <h3>Free your mind and imagination!</h3>
+        </div>
+    )
+}
+
+export default Footer
