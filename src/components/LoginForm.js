1535139e7a8aedbb699ee299f59d49fad8fcbade
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-29 11:27:17 -0300

    added proptypes definition to the component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,21 +1,36 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const LoginForm = (props) => {
+const LoginForm = ({
+    handleLogin,
+    handleUsernameChange,
+    handlePasswordChange,
+    username,
+    password
+}) => {
     return (
         <div>
-            <form onSubmit={props.handleLogin}>
+            <form onSubmit={handleLogin}>
                 <div>
                     username
-                    <input type="text" value={props.username} onChange={props.handleUsernameChange} />
+                    <input type="text" value={username} onChange={handleUsernameChange} />
                 </div>
                 <div>
                     password
-                    <input type="password" value={props.password} onChange={props.handlePasswordChange} />
+                    <input type="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 <button type="submit">Login</button>
             </form>
         </div>
     )
+}
+
+LoginForm.propTypes = {
+    handleLogin: PropTypes.func.isRequired,
+    handleUsernameChange: PropTypes.func.isRequired,
+    handlePasswordChange: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
 }
 
 export default LoginForm
